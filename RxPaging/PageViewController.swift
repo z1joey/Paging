8d1ee2c7bea7cdd7c711b06bc94788a897e9aa1b
Joey Zhang <joey.zhang@tgitech.net>
2019-11-26 00:43:12 +0800

    feat: init rxpaging target

---
+++ RxPaging/PageViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PageViewController.swift
+//  RxPaging
+//
+//  Created by Yi Zhang on 2019/11/26.
+//  Copyright © 2019 TGI Technology. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    lazy var pages: [UIViewController] = {
+        let red = UIViewController()
+        red.view.backgroundColor = .red
+        
+        let yellow = UIViewController()
+        yellow.view.backgroundColor = .yellow
+        
+        return [red, yellow]
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.delegate = self
+        self.dataSource = self
+
+        if let firstVC = pages.first {
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+}
+
+extension PageViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        if let index = pages.firstIndex(of: viewController), index > 0 {
+            return pages[index - 1]
+        }
+        return nil
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        if let index = pages.firstIndex(of: viewController), index < pages.count - 1 {
+            return pages[index + 1]
+        }
+        return nil
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        if let index = pages.firstIndex(of: pendingViewControllers.first!), index < pages.count {
+            //currentIndexPath = IndexPath(item: index, section: 0)
+        }
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            
+        }
+    }
+
+}
---
+++ RxPaging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxPaging
+//
+//  Created by Yi Zhang on 2019/11/26.
+//  Copyright © 2019 TGI Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
